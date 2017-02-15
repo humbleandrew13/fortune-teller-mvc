887f459141f7e54840b23f762d6b622c2a7c35b9
Andy Humble <humbleandrew13@gmail.com>
2017-02-15 13:58:38 -0500

    Tuned details page

--- fortune-teller-mvc/Controllers/CustomersController.cs
+++ fortune-teller-mvc/Controllers/CustomersController.cs
@@ -33,6 +33,12 @@ namespace fortune_teller_mvc.Controllers
             {
                 return HttpNotFound();
             }
+
+            Random randomNum = new Random();
+            ViewBag.RetirementAge = (randomNum.Next(65, 97));
+            ViewBag.VacationHome = "Bermuda";
+            ViewBag.Transportation = "Lamborghini";
+            ViewBag.MoneyInBank = (randomNum.Next(10000, 1000001));
             return View(customer);
         }
 
